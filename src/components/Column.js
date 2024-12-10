cd1781ae555a22db36048126e4d993e18db57b0f
Adhithya <107809749+CSKIronMan17@users.noreply.github.com>
2024-12-10 14:44:02 +0530

    Add files via upload

---
+++ src/components/Column.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Droppable } from 'react-beautiful-dnd';
+import Task from './Task';
+
+// Program to display a column with droppable functionality
+
+const Column = ({ columnId, columnName, tasks }) => {
+  return (
+    <Droppable droppableId={columnId}>
+      {(provided) => (
+        <div className="column" {...provided.droppableProps} ref={provided.innerRef}>
+          <h2>{columnName}</h2>
+          {tasks.map((task, index) => (
+            <Task key={task.id} task={task} index={index} />
+          ))}
+          {provided.placeholder}
+        </div>
+      )}
+    </Droppable>
+  );
+};
+
+export default Column;
---
+++ src/components/Task.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Draggable } from 'react-beautiful-dnd';
+
+// Program to display a task with draggable functionality
+
+const Task = ({ task, index }) => {
+  return (
+    <Draggable draggableId={task.id} index={index}>
+      {(provided) => (
+        <div
+          className="task"
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+        >
+          <h3>{task.title}</h3>
+          <p>{task.description.length > 50 ? `${task.description.substring(0, 50)}...` : task.description}</p>
+        </div>
+      )}
+    </Draggable>
+  );
+};
+
+export default Task;
